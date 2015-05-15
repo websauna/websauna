d3807bb98627184a1d7df5d57c96b9d89ffb0f64
Mikko Ohtamaa <mikko@opensourcehacker.com>
2015-05-15 20:24:32 +0300

    Adding UUID slugs and back

--- pyramid_web20/utils/slug.py
+++ pyramid_web20/utils/slug.py
@@ -1,14 +1,22 @@
+"""URL slug helpers."""
 import uuid
-
+import base64
 
 def uuid_to_slug(uuid_):
     """Create a URL slug from UUID.
 
     Courtesy of MJ: # http://stackoverflow.com/a/12270917/315168
     """
-    return uuid_.bytes.encode('base64').rstrip('=\n').replace('/', '-')
+
+    encoded = base64.b64encode(uuid_.bytes)
+    import ipdb ; ipdb.set_trace()
+
+    # URLs don't like +
+    return encoded.decode("utf-8").rstrip('=\n').replace('/', '_').replace("+", "-")
 
 
 def slug_to_uuid(slug):
     """Convert UUID URL slug to UUID object."""
-    return uuid.UUID(bytes=(slug + '==').replace('-', '/').decode('base64'))
+    bytes = (slug + '==').replace('_', '/').replace("-", "+")
+    bytes = base64.b64decode(bytes)
+    return uuid.UUID(bytes=bytes)

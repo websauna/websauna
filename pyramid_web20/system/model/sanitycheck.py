b29ae75bc5efb9b8178812f2e9d68cb03a6743be
Mikko Ohtamaa <mikko@opensourcehacker.com>
2015-06-04 14:20:30 -0700

    Bump SQLAlchemy. Make sure we correctly sanity check declared_attr.

--- pyramid_web20/system/model/sanitycheck.py
+++ pyramid_web20/system/model/sanitycheck.py
@@ -2,6 +2,7 @@ import logging
 
 from sqlalchemy import inspect
 from sqlalchemy.ext.declarative.clsregistry import _ModuleMarker
+from sqlalchemy.orm import RelationshipProperty
 
 logger = logging.getLogger(__name__)
 
@@ -9,7 +10,11 @@ logger = logging.getLogger(__name__)
 def is_sane_database(Base, session):
     """Check whether the current database matches the models declared in model base.
 
-    Currently we check that all tables exist with all columns. Column types are not verified.
+    Currently we check that all tables exist with all columns. What is not checked
+
+    * Column types are not verified
+
+    * Relationships are not verified at all (TODO)
 
     :param Base: Declarative Base for SQLAlchemy models to check
 
@@ -39,10 +44,17 @@ def is_sane_database(Base, session):
 
             columns = [c["name"] for c in iengine.get_columns(table)]
             mapper = inspect(klass)
-            for column in mapper.attrs:
-                if not column.key in columns:
-                    logger.error("Model %s declares column %s which does not exist in database %s", klass, column.key, engine)
-                    errors = True
+
+            for column_prop in mapper.attrs:
+                if isinstance(column_prop, RelationshipProperty):
+                    # TODO: Add sanity checks for relations
+                    pass
+                else:
+                    for column in column_prop.columns:
+                        # Assume normal flat column
+                        if not column.key in columns:
+                            logger.error("Model %s declares column %s which does not exist in database %s", klass, column.key, engine)
+                            errors = True
         else:
             logger.error("Model %s declares table %s which does not exist in database %s", klass, table, engine)
             errors = True

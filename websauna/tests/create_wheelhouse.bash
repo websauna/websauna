b0cd8bae4a6891398f9fdb68e06c788d891aba5a
Mikko Ohtamaa <mikko@opensourcehacker.com>
2016-01-16 19:24:59 +0200

    Updated verboseness of create_wheelhouse

--- websauna/tests/create_wheelhouse.bash
+++ websauna/tests/create_wheelhouse.bash
@@ -4,23 +4,25 @@
 # Builds a wheelhouse folder containing al dependencies.
 #
 
+set -e
+# set -x
+
 if type virtualenv-3.4 ; then
     VIRTUALENV=virtualenv-3.4
 else
     VIRTUALENV=virtualenv
 fi
 
-set -e
-set -x
+
 rm -rf /tmp/wheelhouse-venv
 rm -rf wheelhouse
 $VIRTUALENV --no-site-packages -p python3.4 /tmp/wheelhouse-venv
 source /tmp/wheelhouse-venv/bin/activate
 # default pip is too old for 3.4
 # https://github.com/jnrbsn/daemonocle/issues/8
-pip install -U pip
-pip install .[test,dev]
-pip install wheel
+pip install -q -U pip
+pip install -q .[test,dev]
+pip install -q wheel
 pip freeze > /tmp/wheelhouse-venv/requirements.txt
 # websauna 0.0 development not available, remove from freeze
 echo "$(grep -v "websauna" /tmp/wheelhouse-venv/requirements.txt)" >/tmp/wheelhouse-venv/requirements.txt

4bba999815b8df4b3388e350dc18cb12aeb35012
Mikko Ohtamaa <mikko@opensourcehacker.com>
2016-01-16 23:23:18 +0200

    Adding missing Codecov dependency

--- websauna/system/task/celery.py
+++ websauna/system/task/celery.py
@@ -1,11 +1,4 @@
 """Wrap pyramid_celery loader, so that it works with our INI includer hack."""
-try:
-    import coverage
-    coverage.process_startup()
-except ImportError:
-    # http://nedbatchelder.com/code/coverage/subprocess.html
-    pass
-
 
 from websauna.utils.configincluder import IncludeAwareConfigParser
 from websauna.utils.configincluder import monkey_patch_paster_config_parser
@@ -15,9 +8,11 @@ monkey_patch_paster_config_parser()
 from pyramid_celery.loaders import INILoader
 INILoader.ConfigParser = IncludeAwareConfigParser
 
+# TODO: replace pyramid_celery with some more decoupled implementatin
 from pyramid_celery import celery_app
 from pyramid_celery import includeme
 
 
-
+# Export pyramid_celery as pass through
+__all__ = ["celery_app", "includeme"]
 

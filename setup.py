f22b1039d496dd65a7bb9e0f55949af1befc826f
Mikko Ohtamaa <mikko@opensourcehacker.com>
2016-01-16 20:18:17 +0200

    Codecov token not required on Travis CI for public repo

--- setup.py
+++ setup.py
@@ -48,8 +48,8 @@ setup(
         'License :: OSI Approved :: MIT License',
 
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
     ],
 
     # What does your project relate to?

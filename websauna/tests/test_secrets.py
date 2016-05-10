5253854507f36d558337bf252519761f8caf4797
Mikko Ohtamaa <mikko@opensourcehacker.com>
2016-05-10 07:41:59 +0300

    Fix test to have no environment variable expansion for secrets.

--- websauna/tests/test_secrets.py
+++ websauna/tests/test_secrets.py
@@ -7,17 +7,17 @@ from websauna.utils import secrets
 def test_read_local_secrets():
     """Read file referred by a relative path."""
     data = secrets.read_ini_secrets("test-secrets.ini")
-    assert data.get("authomatic.secret") == os.environ["RANDOM_VALUE"]
+    assert data.get("authomatic.secret") == "xxx"
 
 
 def test_file_secrets():
     """Read file referred by absolute path."""
     data = secrets.read_ini_secrets("file://" + os.path.abspath("test-secrets.ini"))
-    assert data.get("authomatic.secret") == os.environ["RANDOM_VALUE"]
+    assert data.get("authomatic.secret") == "xxx"
 
 
 def test_read_local_secrets():
     """Read file referred by a local path."""
     data = secrets.read_ini_secrets("test-secrets.ini")
-    assert data.get("authomatic.secret") == os.environ["RANDOM_VALUE"]
+    assert data.get("authomatic.secret") == "xxx"
 

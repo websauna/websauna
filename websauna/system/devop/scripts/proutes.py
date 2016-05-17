aa4947a1d659ef99138d2435c9751cd09c1e5c04
Tom Lazar <tomster@pyfidelity.com>
2016-05-17 16:46:39 +0200

    add forgotten script for PR #36

---
+++ websauna/system/devop/scripts/proutes.py
@@ -0,0 +1,14 @@
+"""Config file includer aware wrapper for proutes."""
+import sys
+from pkg_resources import load_entry_point
+
+from websauna.utils.configincluder import monkey_patch_paster_config_parser
+from websauna.utils.configincluder import IncludeAwareConfigParser
+
+monkey_patch_paster_config_parser()
+
+def main():
+    sys.exit(
+        load_entry_point('pyramid', 'console_scripts', 'proutes')()
+    )
+

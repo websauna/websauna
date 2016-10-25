0f92e1cf7a2a053700735e075fb383bc2389fdea
Mikko Ohtamaa <mikko@opensourcehacker.com>
2016-10-25 11:05:50 -0400

    Blacklist ethereum py.test plugin. Fix CSS class changes introduced by Deform 2.0

--- websauna/tests/test_scaffold_app.py
+++ websauna/tests/test_scaffold_app.py
@@ -72,7 +72,7 @@ def test_pyramid_debugtoolbar(app_scaffold, dev_db, browser):
         b.visit("http://localhost:6543/error-trigger")
 
         # See that Wertzkreug debugger is up
-        assert b.is_element_present_by_css(".errormsg")
+        assert b.is_element_present_by_css(".traceback")
 
     finally:
         execute_venv_command("ws-pserve myapp/conf/development.ini --stop-daemon --pid-file=test_pserve.pid", app_scaffold, cd_folder="myapp")

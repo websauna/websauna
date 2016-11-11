d20ea1b4ff3ece76dadccc83ec071639b780c2ac
Mikko Ohtamaa <mikko@opensourcehacker.com>
2016-11-12 01:02:39 +0200

    Add better support for JSON widgets.

--- websauna/system/form/widgets.py
+++ websauna/system/form/widgets.py
@@ -2,8 +2,10 @@
 
 Mostly for high level integration.
 """
+import json
 
 import deform
+from colander import null
 from deform.widget import _normalize_choices
 from websauna.utils.slug import uuid_to_slug
 
@@ -28,8 +30,51 @@ JSON_PREFORMATTED_STYLE = "font-family: monospace"
 
 class JSONWidget(deform.widget.TextAreaWidget):
     """Nice JSON editor.
+
+    Prettyprints outgoing JSON for better readability.
+
+    Example usage:
+
+    .. code-block:: python
+
+        import colander
+
+        from websauna.system.form.fields import JSONValue
+        from websauna.system.form.widgets import JSONWidget
+
+        class MySchema(colander.Schema):
+
+            other_data = colander.SchemaNode(
+                JSONValue(),
+                widget=JSONWidget(),
+                description="JSON bag of attributes of the object",
+                missing=dict)
     
     """
 
-    def __init__(self, cols=80, rows=10, style=JSON_PREFORMATTED_STYLE, **kwargs):
+    readonly_template = 'readonly/json'
+
+    def __init__(self, cols=80, rows=10, style=JSON_PREFORMATTED_STYLE, prettyprint=True, **kwargs):
         super(JSONWidget, self).__init__(cols=cols, rows=rows, style=style, **kwargs)
+        self.prettyprint = prettyprint
+
+    def process_prettyprint(self, cstruct: str):
+        if self.prettyprint:
+            try:
+                cstruct = json.loads(cstruct)
+                return json.dumps(cstruct, sort_keys=True, indent=4)
+            except Exception as e:
+                pass
+
+        return cstruct
+
+    def serialize(self, field, cstruct, **kw):
+        if cstruct in (null, None):
+            cstruct = ''
+        readonly = kw.get('readonly', self.readonly)
+        template = readonly and self.readonly_template or self.template
+        values = self.get_template_values(field, cstruct, kw)
+
+        values["cstruct"] = self.process_prettyprint(values["cstruct"])
+        return field.renderer(template, **values)
+

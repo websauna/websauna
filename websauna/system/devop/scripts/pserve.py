6ce45deca25859ae8ce3c80835d2982047dd4a93
Antti Haapala <antti.haapala@anttipatterns.com>
2017-08-28 19:47:06 +0300

    remove monkey patching as it is unneeded.

--- websauna/system/devop/scripts/pserve.py
+++ websauna/system/devop/scripts/pserve.py
@@ -8,11 +8,6 @@ try:
 except ImportError:
     pass
 
-from websauna.utils.configincluder import monkey_patch_paster_config_parser
-from websauna.utils.configincluder import IncludeAwareConfigParser
-
-monkey_patch_paster_config_parser()
-
 
 def main():
     sys.exit(

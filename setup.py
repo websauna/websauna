a5d97c0e74c158bc454b817f84e28a6822f14a25
Érico Andrei <ericof@gmail.com>
2018-04-30 10:59:56 +0200

    Back to development: 1.0a10

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 setup(
     name='websauna',
     namespace_packages=["websauna"],
-    version='1.0a9',
+    version='1.0a10.dev0',
     description=long_description.split("\n")[0],
     long_description=long_description,
     url='https://websauna.org',

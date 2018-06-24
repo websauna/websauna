5d49d9b81e8392a1adbb123169faa525c6e1c9c8
Érico Andrei <ericof@gmail.com>
2018-06-24 23:06:40 +0200

    [WIP] Remove block to Python 3.7

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
     packages=find_packages(exclude=['docs']),
     include_package_data=True,
     zip_safe=False,
-    python_requires='>=3.5.2,<3.7',
+    python_requires='>=3.5.2',
     install_requires=[
         # Pyramid dependencies
         'pyramid>=1.9',

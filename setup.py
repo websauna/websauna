deff4cdfb708eec33351fddf684482d0a69ec850
Érico Andrei <ericof@gmail.com>
2018-06-24 23:21:39 +0200

    Support Python 3.7

--- setup.py
+++ setup.py
@@ -34,6 +34,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: Implementation :: CPython',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',

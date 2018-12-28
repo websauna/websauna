f13aa77bc75bd9abfb7e2c3b0f2a2188513b0e10
Érico Andrei <ericof@gmail.com>
2018-12-28 10:21:54 -0200

    Improve is_sane_database to deal with column_property and non declarative objects (#218)

--- websauna/system/model/sanitycheck.py
+++ websauna/system/model/sanitycheck.py
@@ -1,8 +1,10 @@
 # Standard Library
 import logging
+import typing as t
 
 # SQLAlchemy
 import sqlalchemy
+from sqlalchemy import Column
 from sqlalchemy import inspect
 from sqlalchemy.ext.declarative.clsregistry import _ModuleMarker
 from sqlalchemy.orm import RelationshipProperty
@@ -12,19 +14,29 @@ from sqlalchemy.orm import Session
 logger = logging.getLogger(__name__)
 
 
+def _log_error_message(klass: object, engine: object, table: t.Optional[str] = None, column: t.Optional[str] = None):
+    instance = 'column' if column else 'table'
+    name = column if column else table
+    logger.error(
+        "Model {klass} declares {instance} {name} which does not exist in database {engine}".format(
+            klass=klass,
+            instance=instance,
+            name=name,
+            engine=engine
+        )
+    )
+
+
 def is_sane_database(Base, session: Session):
     """Check whether the current database matches the models declared in model base.
 
     Currently we check that all tables exist with all columns. What is not checked
 
-    * Column types are not verified
-
-    * Relationships are not verified at all (TODO)
+        * Column types are not verified
+        * Relationships are not verified at all (TODO)
 
     :param Base: Declarative Base for SQLAlchemy models to check
-
     :param session: SQLAlchemy session bound to an engine
-
     :return: True if all declared models have corresponding tables and columns.
     """
 
@@ -46,17 +58,14 @@ def is_sane_database(Base, session: Session):
         # 1. Check if this model declares any tables they exist
 
         # First try raw table definition
-        __table__ = getattr(klass, "__table__", None)
-        if __table__ is not None:
-            table = __table__.name
-        else:
-            table = getattr(klass, "__tablename__", None)
+        table = getattr(klass, "__table__", None)
+        table = table.name if table is not None else getattr(klass, "__tablename__", None)
 
         if not table:
-            raise RuntimeError("Table definition missing for {}".format(table))
+            raise RuntimeError("Table definition missing for {name}".format(name=klass.__name__))
 
         if table not in tables:
-            logger.error("Model %s declares table %s which does not exist in database %s", klass, table, engine)
+            _log_error_message(klass, engine, table)
             errors = True
 
         # 2. Check that all columns declared by the model exist
@@ -69,19 +78,23 @@ def is_sane_database(Base, session: Session):
 
             # Iterate all columns the model declares
             for column in column_prop.columns:
+                # Just deal with sqlalchemy.Column instances
+                if not isinstance(column, Column):
+                    continue
+
                 table_name = column.table.name
 
                 # Get columns from the actual table the ORM referes to
                 try:
                     columns = [c["name"] for c in iengine.get_columns(table_name)]
                 except sqlalchemy.exc.NoSuchTableError:
-                    logger.error("Model %s declares table %s which does not exist in database %s", klass, table_name, engine)
+                    _log_error_message(klass, engine, table_name)
                     errors = True
                     break
 
                 if column.key not in columns:
                     # It is safe to stringify engine where as password should be blanked out by stars
-                    logger.error("Model %s declares column %s which does not exist in database %s", klass, column.key, engine)
+                    _log_error_message(klass, engine, None, column.key)
                     errors = True
 
     return not errors
--- websauna/tests/devop/test_database_sanity_check.py
+++ websauna/tests/devop/test_database_sanity_check.py
@@ -5,12 +5,16 @@ from sqlalchemy import Column
 from sqlalchemy import ForeignKey
 from sqlalchemy import Integer
 from sqlalchemy import String
+from sqlalchemy import Table
 from sqlalchemy import engine_from_config
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.ext.declarative import declared_attr
 from sqlalchemy.ext.hybrid import hybrid_property
+from sqlalchemy.orm import column_property
 from sqlalchemy.orm import relationship
 from sqlalchemy.orm import sessionmaker
+
+import pytest
 
 # Websauna
 from websauna.system.model.sanitycheck import is_sane_database
@@ -34,6 +38,9 @@ def gen_test_model():
 
         #: Running counter used in foreign key references
         id = Column(Integer, primary_key=True)
+
+        #: Add column property
+        prefix_id = column_property("model-" + id)
 
     return Base, SaneTestModel
 
@@ -73,6 +80,31 @@ def gen_declarative():
             return self._password
 
     return Base, DeclarativeTestModel
+
+
+def gen_non_declarative_table():
+
+    Base = declarative_base()
+
+    class DeclarativeTestModel(Base):
+        __tablename__ = "sanity_check_test_5"
+        id = Column(Integer, primary_key=True)
+
+        @declared_attr
+        def _password(self):
+            return Column('password', String(256), nullable=False)
+
+        @hybrid_property
+        def password(self):
+            return self._password
+
+    table = Table(
+        'sanity_check_test_6',
+        Base.metadata,
+        Column('id', Integer, primary_key=True)
+    )
+
+    return Base, [DeclarativeTestModel.__table__, table]
 
 
 def test_sanity_pass(ini_settings, dbsession):
@@ -184,3 +216,57 @@ def test_sanity_pass_declarative(ini_settings, dbsession):
         assert is_sane_database(Base, session) is True
     finally:
         Base.metadata.drop_all(engine)
+
+
+def test_sanity_fail_if_table_definition_is_missing(ini_settings, dbsession):
+    """See database sanity check raises a RuntimeError if table definition is missing."""
+
+    engine = engine_from_config(ini_settings, 'sqlalchemy.')
+    conn = engine.connect()
+    conn.begin()
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    Base, DeclarativeTestModel = gen_declarative()
+    try:
+        Base.metadata.drop_all(engine, tables=[DeclarativeTestModel.__table__])
+    except sqlalchemy.exc.NoSuchTableError:
+        pass
+
+    Base.metadata.create_all(engine, tables=[DeclarativeTestModel.__table__])
+
+    # Delete __tablename__ and __table__ attributes to force a table definition missing error.
+    del(DeclarativeTestModel.__tablename__)
+    del(DeclarativeTestModel.__table__)
+
+    with pytest.raises(RuntimeError) as exc:
+        is_sane_database(Base, session)
+
+    assert exc.value.args[0] == "Table definition missing for DeclarativeTestModel"
+    Base.metadata.drop_all(engine)
+
+
+def test_sanity_pass_with_non_declarative_objects(ini_settings, dbsession):
+    """See database sanity check does not complain about non declarative objects."""
+
+    engine = engine_from_config(ini_settings, 'sqlalchemy.')
+    conn = engine.connect()
+    conn.begin()
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    Base, tables = gen_non_declarative_table()
+
+    try:
+        Base.metadata.drop_all(engine, tables=tables)
+    except sqlalchemy.exc.NoSuchTableError:
+        pass
+
+    Base.metadata.create_all(engine, tables=tables)
+
+    try:
+        assert is_sane_database(Base, session) is True
+    finally:
+        Base.metadata.drop_all(engine, tables=tables)
